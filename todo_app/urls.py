2a9d9878fdefbd2125a10e48f8e7ab45b067d36f
TimaDootaliev <timur.dootaliev.td@gmail.com>
2023-03-16 20:09:42 +0600

    swagger

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import get_todos, create_todo, update_todo, delete_todo
+from .views import get_todos, create_todo, update_todo, delete_todo, get_todo
 
 urlpatterns = [
     path('todos/', get_todos),
     path('create-todo/', create_todo),
     path('update-todo/<int:pk>/', update_todo),
-    path('delete-todo/<int:pk>/', delete_todo)
+    path('delete-todo/<int:pk>/', delete_todo),
+    path('todo/<int:pk>/', get_todo)
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -2,21 +2,21 @@ from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import get_object_or_404
+
+from drf_yasg.utils import swagger_auto_schema
 
 from .models import ToDo
 from .serializers import ToDoSerializer, UpdateToDoSerializer
 
 
 @api_view(['GET'])
-def get_todos(request: Request):
-    queryset = ToDo.objects.all()
-    # SELECT * FROM todo -> QuerySet([{title..., 'decs,..}])
-    print(queryset)
+def get_todos(request: Request) -> Response:
+    queryset = ToDo.objects.all() # SELECT * FROM todo -> QuerySet([{title..., 'decs,..}])
     serializer = ToDoSerializer(queryset, many=True)
-    print(serializer.data)
     return Response(serializer.data, status=status.HTTP_200_OK)
 
-
+@swagger_auto_schema(method='POST', request_body=ToDoSerializer)
 @api_view(['POST'])
 def create_todo(request: Request) -> Response:
     serializer = ToDoSerializer(data=request.data)
@@ -30,6 +30,7 @@ def create_todo(request: Request) -> Response:
     #     return Response(serializer.data, status=status.HTTP_201_CREATED)
     # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+@swagger_auto_schema(method='PATCH', request_body=UpdateToDoSerializer)
 @api_view(['PATCH'])
 def update_todo(request: Request, pk) -> Response:
     print(pk)
@@ -56,5 +57,11 @@ def delete_todo(request: Request, pk) -> Response:
     return Response({'detail': 'Successfully deleted'}, status=status.HTTP_204_NO_CONTENT)
 
 
-# TODO: написать функцию для получения ОДНОГО объекта
-# TODO: подключить Swagger
+@api_view(['GET'])
+def get_todo(request: Request, pk) -> Response:
+    todo = get_object_or_404(ToDo, pk=pk)
+    serializer = ToDoSerializer(instance=todo)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+
